ce460fddab5bb1df31edf7dc4a13a6007e721f19
solomonbroadbent <solomonbroadbentnz@gmail.com>
2016-10-25 17:33:00 +1300

    Removed invisible spans from index.html and refactored code in imageHover.js to automatically add them. Begun refactoring changeOverlayStatus()

--- js/controllers/imageHover.js
+++ js/controllers/imageHover.js
@@ -1,9 +1,25 @@
-function changeOverlayStatus(imageWhichOverlays, shouldOverlay){
-    if(shouldOverlay == true){
-        document.getElementById(imageWhichOverlays).style.visibility = 
-            'visible';
-    }
-    else{
-        document.getElementById(imageWhichOverlays).style.visibility = 'hidden';
-    }
+function changeOverlayStatus(overlayImageId){
+    $("#" + overlayImageId + "").toggle();
 }
+//refactoring TODO:
+/*  1: select all bd_upper
+    2: insert a span above which:
+    	a: contains the image tag from the bd_upper span
+    	b: has the same heirachy in the DOM as bd_upper
+    	c: has class bd_imageSpan and bd_invisible
+    3: add event listeners to each span which changeOverlayStatus() on the id of
+		the bd_upper span
+*/
+$(document).ready(function(){
+  	$(".bd_upper").each(function(){
+		var idUpper = $(this).attr('id');
+		var containedImage = $(this).html();
+		var idInvisible = idUpper + "_addedInvisibleLayer";
+		var invisibleLayer = `<span id='` + idInvisible + `'
+			onmouseenter='changeOverlayStatus(` + idUpper + `)'
+			onmouseleave='changeOverlayStatus(` + idUpper + `)'>`
+			+ containedImage + `</span>`;
+		$(this).before(invisibleLayer);
+		$("#" + idInvisible).addClass("bd_imageSpan bd_invisible");
+	})
+});
